16c51cf6ba7529f94faf9d436dd71a8204d0f643
akbarattar3 <akbar@mobifyi.com>
2022-04-19 16:33:35 +0530

    restructure

---
+++ app/globals/utils/Display.js
@@ -0,0 +1,6 @@
+import {Dimensions} from 'react-native';
+
+const {height, width} = Dimensions.get('window');
+
+export const setHeight = h => (height / 100) * h;
+export const setWidth = w => (width / 100) * w;
